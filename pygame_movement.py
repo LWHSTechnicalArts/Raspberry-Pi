e440c1fe450f4c0c516758e27b7900f8c28b4fdf
Andrew Kleindolph <akleindolph@gmail.com>
2023-11-13 15:02:23 -0800

    Update pygame_movement.py

--- pygame_movement.py
+++ pygame_movement.py
@@ -12,6 +12,7 @@ dt = 0
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 RED = (255,0,0)
 PURPLE = (70,0,100)
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
