2054a79e3fd7fc45a1d9f011e3d999edf12fd372
Andrew Kleindolph <akleindolph@gmail.com>
2023-11-22 12:04:20 -0800

    Update temp_humid.py

--- temp_humid.py
+++ temp_humid.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 """
 Basic `AHTx0` example test
 """
@@ -11,10 +8,12 @@ import adafruit_ahtx0
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_ahtx0.AHTx0(i2c)
 
 while True:
-    print("\nTemperature: %0.1f C" % sensor.temperature)
-    print("Humidity: %0.1f %%" % sensor.relative_humidity)
-    time.sleep(2)
+    temperature = round(sensor.temperature,2)
+    relative_humidity = round(sensor.relative_humidity,2)
+    
+    print("\nTemperature: %0.1f C" % temperature)
+    print("Humidity: %0.1f %%" % relative_humidity)
+    time.sleep(5)
