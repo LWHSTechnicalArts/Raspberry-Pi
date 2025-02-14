c71889d7f5e3859f7295a2002e2e3ffd002e1eef
Andrew Kleindolph <akleindolph@gmail.com>
2025-02-14 13:19:14 -0800

    Update weather_led.py

--- weather_led.py
+++ weather_led.py
@@ -12,6 +12,7 @@ settings = {
     'temp_unit':'imperial'} #unit can be metric, imperial, or kelvin
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?appid={0}&zip={1},{2}&units={3}"
+
 while True:
     final_url = BASE_URL.format(settings["api_key"],settings["zip_code"],settings["country_code"],settings["temp_unit"])
     weather_data = requests.get(final_url).json()
