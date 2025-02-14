efc7cd66860e759e7262be4ef6ab9c3d38140aef
Andrew Kleindolph <akleindolph@gmail.com>
2025-02-14 13:11:37 -0800

    Update history_api_4.py

--- history_api_4.py
+++ history_api_4.py
@@ -1,6 +1,6 @@
 import requests
 from requests import get
 
-url = 'http://history.muffinlabs.com/date'
-one_day = get(url).json() 
+url = 'http://history.muffinlabs.com/date/12/25'
+one_day = get(url, verify=False).json() 
 print (one_day['data']['Events'][-1]['year'],one_day['data']['Events'][-1]['text'])
