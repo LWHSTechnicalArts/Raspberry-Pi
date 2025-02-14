65e13adece4c590ec241c3ad3b1b6015b2fb4860
Andrew Kleindolph <akleindolph@gmail.com>
2025-02-14 13:14:39 -0800

    Update history_api_5.py

--- history_api_5.py
+++ history_api_5.py
@@ -1,12 +1,11 @@
 import time
 import random
-import requests
 from requests import get
 
 url = 'http://history.muffinlabs.com/date'
 
 while True:
   item = random.randint(-10,10)
-  one_day = get(URL, verify=False).json()
+  one_day = get(url, verify=False).json()
   print (one_day['data']['Events'][item]['year'],one_day['data']['Events'][item]['text'])
   time.sleep(20)
